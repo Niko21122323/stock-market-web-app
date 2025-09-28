02b52d09decd924f7e425854b6b88d8a41ac324d
Nikola <nikola211223232529321@gmail.com>
2025-09-29 00:52:36 +0200

    started working on my file structure and created the header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+	return (
+		<div className="flex min-h-screen home-wrapper">
+			<h1>Home</h1>
+		</div>
+	);
+};
+
+export default Home;
